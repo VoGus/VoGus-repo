05cd94ebd4e9bed52354ba99b67b534b4bd196e7
VoGus <voguslol@mail.ru>
2014-09-28 14:45:26 +0400

    test 2 added

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+/**
+ * Created by VoGus on 28.09.2014.
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
--- README.txt
+++ README.txt
@@ -1,1 +1,1 @@
-lol
+java coding :D
