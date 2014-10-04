9885dc1d4a9d1274a7fa222f9301ddb39fa98a5c
VoGus <voguslol@mail.ru>
2014-10-04 19:14:14 +0400

    Fix+Add

--- Hospital/Doctor.java
+++ Hospital/Doctor.java
@@ -1,7 +1,19 @@
 package Hospital;
 
+import java.util.Scanner;
+
 /**
  * Created by VoGus on 28.09.2014.
  */
 public class Doctor extends Staff {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int i = 90, j = 30000, z=0, x=0, y=0;
+    System.out.print(i + " врачей нуждаются в курсах повышения квалификации.\nСколько денег Вы готовы выделить на их обучение?\n");
+    z = sc.nextInt();
+    x=z/j;
+        System.out.println(x + " врачей прошло курсы.");
+    y=i-x;
+            System.out.println(y + " врачей всё ещё нуждаются в курсах.");
+    }
 }
--- Hospital/Staff.java
+++ Hospital/Staff.java
@@ -1,12 +1,11 @@
 package Hospital;
 import java.util.Scanner;
-import javax.swing.Timer;
+
 /**
  * Created by VoGus on 28.09.2014.
  */
 public class Staff {
     public static void main(String[] args) {
-        java.util.Timer timer2 = new java.util.Timer();
         Scanner sc = new Scanner(System.in);
         int i = 0, j = 200;
         System.out.print("У вас 200 человек персонала. Укажите среднюю зарплату: ");
