70b8cef8c4014f09f34b396e31280889fe9332d7
VoGus <voguslol@mail.ru>
2014-10-04 18:53:16 +0400

    Added new things

--- Hospital/Patients.java
+++ Hospital/Patients.java
@@ -1,7 +1,19 @@
 package Hospital;
-
+import java.util.Scanner;
 /**
  * Created by VoGus on 28.09.2014.
  */
 public class Patients {
-}
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int i = 0;
+            System.out.print("Введите количество пациентов: ");
+            i = sc.nextInt();
+            if(i<=10) {
+                System.out.println("Все хорошо!");
+            } else {
+                System.out.println("Много больных :(");
+            }
+        }
+    }
+
--- Hospital/Staff.java
+++ Hospital/Staff.java
@@ -1,7 +1,21 @@
 package Hospital;
-
+import java.util.Scanner;
+import javax.swing.Timer;
 /**
  * Created by VoGus on 28.09.2014.
  */
 public class Staff {
+    public static void main(String[] args) {
+        java.util.Timer timer2 = new java.util.Timer();
+        Scanner sc = new Scanner(System.in);
+        int i = 0, j = 200;
+        System.out.print("У вас 200 человек персонала. Укажите среднюю зарплату: ");
+        i = sc.nextInt();
+        if (i < 20000) {
+            for (j = 200; j >= Math.random()*150+30; j--)
+                System.out.println(j + " человек персонала осталось у вас");
+        } else {
+            System.out.println("Персонал доволен!");
+        }
+    }
 }
